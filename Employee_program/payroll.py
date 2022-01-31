dfd672ed725739f8e5f4cf2c8f190dc7058bb0e4
Nicolas Child <nickchild14@gmail.com>
2022-01-31 15:27:59 -0700

    Fixed the employees program
    Added my employees program and made it work

--- Employee_program/payroll.py
+++ Employee_program/payroll.py
@@ -1,216 +1,159 @@
-"""
-UVU CS Dept.
-CS1410 Project 5Part2 - Employee
-Summer1 - 2020
-
-
-
-I declare that the following source code was modified solely by me.
-I understand that copying any source code verbatim, in whole, constitues cheating
-and copying in part requires that I acknowledge the author of the original content.
-In this case these presentations are in general authored by Dana Doggett and
-Chuck Allison as part of the UVU CS department for instructional purposes.
-
-
-"""
-#todo:
-
-#completed:
-# 1. Implement Employee
-# 2. Implement Classification and Classification Types
-# 3. Implement PaymentMethod and Method Types
-# 4. Implement load_employees
-# 5. Implement find_employee_by_id
-# 6. Implement process_timecards
-# 7. Implement process_receipts
-# 8. run_payroll
-# 9. run main
-class Employee:
-    '''develops a payroll program for a company'''
-    def __init__(self, emp_id, name, address, city, state, zipcode, classification=None, payment_Method=None):
-        '''strings to define an employee object except for classification and paymentMethod'''    
-        self.emp_id=emp_id
-        self.name = name
-        self.address=address
-        self.city=city
-        self.state=state
-        self.zipcode=zipcode
-        self.classification=classification
-        self.payment_Method=payment_Method
-    
-    def make_salaried(self, salary):
-        '''sets classification to salary'''
-        self.classification=Salaried(salary)
-     
-    def make_hourly(self, hourly_rate):
-        '''sets classification to hourly'''
-        self.classification=Hourly(hourly_rate)
-
-    def make_commissioned(self, salary, commission_rate):
-        '''sets classification to commissioned'''
-        self.classification = Commissioned(salary)
-    
-    def mailed(self):
-        '''employee has pay mailed to them'''
-        self.pay = Mailed(self)
-    
-    def direct(self, route, account):
-        '''empolyee direct deposits to bank'''
-        self.pay=Direct(self, route, account)
-    
-    def issue_Pay(self):
-        amount=self.classification.get_Pay()
-        self.pay(amount)
-        
+import os, os.path, shutil
 from abc import ABC, abstractmethod
-class Classification(ABC):
-    '''from ABS for an abstract class determine how the employee wants to be paid'''
-
-    @abstractmethod
-    def get_Pay(self):
-        pass
- 
-class Hourly(Classification):
-    '''employee type'''
-    def __init__(self, hourly_rate):
-        self.hourly_rate = hourly_rate
-        self.timecards = []
-        
-    def get_Pay(self):
-        '''calculate pay for hourly employee'''
-        amount = 0.0
-        for timecard in self.timecards:
-            amount += timecard * self.hourly_rate
-        self.timecards.clear()
-        return amount
-
-    def add_Timecard(self, hours):
-        '''add some hours to the timecards list'''
-        self.timecards.append(hours)
-            
-        
-class Salaried(Classification):
-    '''employee type'''
-    def __init__(self, salary):
-        self.salary=salary
-    def get_Pay(self):
-        '''return pay calculation for Salary employee'''
-        return self.salary/24
-    
-        
-
-class Commissioned(Salaried):
-    '''employee type'''
-    def ___init__(self, salary, commission_rate):
-        super().__init__(salary)
-        self.commission_rate = commission_rate
-        self.receipts = []
-
-    def add_Sale(self, receipt):
-        '''put receipts in a list'''
-        self.receipts.append(receipt)
-
-    def get_Pay(self):
-        '''custom pay method'''
-        amount = super().get_Pay()
-        for receipt in self.receipts:
-            amount += receipt * self.commission_rate/ 100
-            self.receipts.clear()
-            return amount
-
-class Payment_Method(ABC):
-    '''abstract PaymentMethod class'''
-    def __init__(self, employee):
-        self.employee = employee
-        
-    @abstractmethod
-    def pay(self, amount):
-        pass
-
-class Direct(Payment_Method):
-    '''employee wants a directdeposit'''
-    def __init__(self, employee, route, account):
-        super().__init__(employee)
-        self.route = route
-        self.account=account
-    def pay(self, amount):
-        with open(pay_log_file, 'a') as plog:#add file to print to. 
-            print("Transferring", f"{amount:.02f}", "for", self.employee.name, "to", self.account, ": ", self.route, file=plog)
-    
-class Mailed(Payment_Method):
-    '''employee wants a mailed check'''
-    def __init__(self, employee):
-        super().__init__(employee)
-
-    def pay(self, amount):
-        with open(pay_log_file, 'a') as plog:
-            print("Mailing", f"{amount:.02f}", "to", self.employee.name, "at" ,self.employee.address, self.employee.city. self.employee.state, self.employee.zipcode, file=plog)
-    
-  
 employees = []
-def load_employees():
-    '''populate employees list'''    
-    with open("employees.csv") as femp:
-        femp.readline()
-        for line in femp:
-            data = line.strip().split(",")
-            emp = Employee(data[0], data[1], data[2], data[3], data[4], data[5])
-            if int(data[6] == 1):
-                emp.make_hourly(float(data[9]))
-            elif int(data[6] == 2):
-                emp.make_salaried(float(data[8]))
-            else:
-                emp.make_commissioned(float(data[8]), int(data[10]))
-        if int(data[7]) == 1:
-            emp.direct(data[11], data[12])
-        else:
-            emp.mailed()
-        employees.append(emp)
+hours = {}
+receipts = {}
+PAY_LOGFILE = "paylog.txt"
+
+class Employee:
+    def __init__(self, ID, first_name, last_name, address, city, state, zipp, num):
+        self.ID = ID
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip = zipp
+        self.num = num
+        # Hourly = 3, comissioned = 2, salary = 1
+    def make_hourly(self, pay):
+        self.hourly = pay
+        self.num = 3
+        if self.ID not in hours:
+            hours[f"{self.ID}"] = []
+        self.classification = Hourly(self.ID)
         
-def find_employee_by_id(emp_id):
-    for emp in employees:
-        if emp.emp_id == emp_id:
-            return emp
-            
+    def make_salaried(self, pay):
+        self.salary = pay
+        self.num = 1
+        self.classification = Salaried()
+
+    def make_comissioned(self, pay, percent):
+        self.salary = pay
+        self.comission = percent
+        self.num = 2
+        if self.ID not in receipts:
+            receipts[f"{self.ID}"] = []
+        self.classification = Comissioned(self.ID)
+
+    def issue_payment(self):
+        payment = Classification.classification_checker(self, self)
+        payroll = round(payment, 2)
+        if self.num == 3:
+            hours[f"{self.ID}"] = []
+        if self.num == 2:
+            receipts[f"{self.ID}"] = []
+        if int(payroll) == 0:
+            pass
+        else:
+            with open("paylog.txt", 'a+') as f:
+                f.write(f"""Mailing {payroll} to {self.first_name} {self.last_name} at {self.address} {self.city} {self.state} {self.zip}\n""")
+        return payroll
+
+def load_employees():
+    global employees
+    with open("employees.csv", 'r') as f:
+        count = 0
+        for lines in f:
+            count += 1
+            if count == 1:
+                pass
+            else:
+                items = lines.split(',')
+                no_new_line = items[-1].strip("\n")
+                items[-1] = no_new_line
+                employee_instance = Employee(items[0], items[1], items[2], items[3], items[4], items[5], items[6], items[7])
+                employees.append(employee_instance)
+                if items[7] == str(3):
+                    employee_instance.make_hourly(items[10])
+                if items[7] == str(1):
+                    employee_instance.make_salaried(items[8])
+                if items[7] == str(2):
+                    employee_instance.make_comissioned(items[8], items[9])
+
+class Classification(ABC):
+    @abstractmethod
+    def issue_pay(self, pay):
+        pass
+    def classification_checker(self, emp_instance):
+        employee = emp_instance
+        if employee.num == 3:
+            return Hourly.issue_pay(self, employee.ID, employee.hourly)
+        if employee.num == 1:
+            return Salaried.issue_pay(self, employee.salary)
+        if employee.num == 2:
+            return Comissioned.issue_pay(self, employee.comission, employee.salary, employee.ID)
+
+class Hourly(Classification):
+    def __init__(self, id):
+        self.id = id
+    def issue_pay(self, id, pay):
+        money = 0
+        hours_list = hours[id]
+        for stuff in hours_list:
+            money += (float(stuff) * float(pay))
+        return money
+    def add_timecard(self, time):
+        hour_list = hours[f'{self.id}']
+        hour_list.append(time)
+        # Makes it so that new id's can be added to dictionary if not present.
+        hours[f'{self.id}'] = hour_list
+
+class Salaried(Classification):
+    def issue_pay(self, pay):
+        return ((1/24) * float(pay))
+    
+
+class Comissioned(Salaried):
+    def __init__(self, id):
+        self.identification = id
+    def issue_pay(self, rate, pay, id):
+        salary = Salaried.issue_pay(self, pay)
+        receipt_list = receipts[id]
+        comission_total = 0
+        for items in receipt_list:
+            comission_total += float(items)
+        comission = comission_total * (float(rate)/ 100)
+        return comission + salary
+    def add_receipt(self, amount):
+        receipt_list = receipts[self.identification]
+        receipt_list.append(amount)
+        receipts[self.identification] = receipt_list
+
 def process_timecards():
-    with open("timecards.txt") as tfile:
-        for line in tfile:
-            data = line.strip().split(",")
-            emp = find_employee_by_id(data[0])
-            if emp != None:
-                data.pop(0)
-                for timecard in data:
-                    emp.classification.add_timecard(float(timecard))
+    with open("timecards.csv", 'r') as f:
+        for line in f:
+            lines = line.split(',')
+            no_new_line = lines[-1].strip('\n')
+            lines[-1] = no_new_line
+            lines = list(filter(None, lines)) 
+            hours[f"{lines[0]}"] = list(lines[1:])
+        with open("timecards.txt", 'w+') as s:
+            s.write(f.read())
 def process_receipts():
-    with open("receipts.txt") as rfile:
-        for line in rfile:
-            data = line.strip().split(",")
-            emp = find_employee_by_id(data[0])
-            if emp != None:
-                data.pop(0)
-                for receipt in data:
-                    emp.classification.add_Sale(float(receipt))
-import os, shutil        
-pay_log_file = 'paylog.txt'
+    with open("receipts.csv", 'r') as f:
+        for line in f:
+            lines = line.split(',')
+            no_new_line = lines[-1].strip('\n')
+            lines[-1] = no_new_line
+            receipts[f"{lines[0]}"] = list(lines[1:])
+def find_employee_by_id(ID):
+    for x in range(len(employees)):
+        if ID == employees[x].ID:
+            return employees[x]
 def run_payroll():
-    if os.path.exists(pay_log_file):
-        os.remove(pay_log_file)
-    for emp in employees:
-        emp.pay()
+    if os.path.exists(PAY_LOGFILE): 
+        os.remove(PAY_LOGFILE)
+    for emp in employees: 
+        emp.issue_payment() 
 
- 
-'''
-    p5.py: Illustrates the payroll module.
-'''
-
-#from payroll import *
 
 def main():
     load_employees()
     process_timecards()
     process_receipts()
     run_payroll()
-
+    pay_log_file = 'paylog.txt'
     # Save copy of payroll file; delete old file
     shutil.copyfile('paylog.txt', 'paylog_old.txt')
     if os.path.exists(pay_log_file):
@@ -219,17 +162,15 @@ def main():
     # Change Karina Gay to Salaried and MailMethod by changing her Employee object:
     emp = find_employee_by_id('688997')
     emp.make_salaried(45884.99)
-    emp.mail_method()
-    emp.issue_Pay()
+    emp.issue_payment()
 
     # Change TaShya Snow to Commissioned and DirectMethod; add some receipts
     emp = find_employee_by_id('522759')
-    emp.make_commissioned(50005.50, 25)
-    emp.direct_method('30417353-K', '465794-3611')
+    emp.make_comissioned(50005.50, 25)
     clas = emp.classification
     clas.add_receipt(1109.73)
     clas.add_receipt(746.10)
-    emp.issue_Pay()
+    emp.issue_payment()
 
     # Change Rooney Alvarado to Hourly; add some hour entries
     emp = find_employee_by_id('165966')
@@ -240,8 +181,7 @@ def main():
     clas.add_timecard(8.0)
     clas.add_timecard(8.0)
     clas.add_timecard(8.0)
-    emp.issue_Pay()
+    emp.issue_payment()
 
 if __name__ == '__main__':
     main()
-
